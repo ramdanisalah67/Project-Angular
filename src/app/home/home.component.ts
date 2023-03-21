7a315359f29af7ef5517914f7341d7cf63149684
ramdanisalah67 <ramdanisalah67@gmail.com>
2023-03-21 02:27:08 +0000

    learn Angular Directives ngIf ngFor ngSwitch

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,47 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class HomeComponent implements OnInit{
-  //==Interpolation==
-  firstName= "ramdani"
-  lastName="salah-eddine"
-  WebSite = "www.winopapa.com"
-  country="tunisia"
-  //declare object
-  Stage={
-    Libelle:"developper un site web par Spring et Angular",
-    type:"en ligne"
+  response=true
+  a=2
+  b=3
+info_users=['omar','salah-eddine','yahya']
+num=0 ;
 
+  constructor() {
   }
-  //==Property Binding==
-  //read image
-  link ="../assets/images/Capture.PNG"
-  div_style="color:red;font-weight:bold;text-transform:uppercase;"
-
-
-//==Evennt Binding==
-number = 1 ;
-
- increment(){
-  this.number ++
- }
-
- decrement(){
-this.number-- ;
- }
-apply="block"
- show(){
-  this.apply="block"
- }
-
-hide(){
-  this.apply="none"
-}
-//==two ways Bindong
-content="this is content"
-
-
-
-  constructor() { }
   ngOnInit(): void {
 
   }
