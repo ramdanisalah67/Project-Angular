4525c31b0b1b9361346ef1b93dd58653293720f1
ramdanisalah67 <ramdanisalah67@gmail.com>
2023-03-22 04:15:39 +0000

    Pipes in Angular

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -12,7 +12,17 @@ export class HomeComponent implements OnInit{
   b=3
 info_users=['omar','salah-eddine','yahya']
 num=0 ;
+my_style_div="mydiv"
+style1="red"
+style2="30px"
+style3="underline"
+etudiant={name:'salah-eddine',age:'25'}
 
+//read json object
+employee={
+ "Id":1,
+  "Name":"salah-eddine"
+}
   constructor() {
   }
   ngOnInit(): void {
