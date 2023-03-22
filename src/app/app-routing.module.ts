951dbd26778a21e2c2ab9c1a887cbab655b0dee1
ramdanisalah67 <ramdanisalah67@gmail.com>
2023-03-22 13:30:16 +0000

    Angular Routing Redirect Route,wildcard Route,Child Route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
+import { DetailsComponent } from './details/details.component';
 import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { ProfilComponent } from './profil/profil.component';
+import { ViewComponent } from './view/view.component';
 
 const routes: Routes = [
 {path:'',component:HomeComponent},
 {path:'about',component:AboutComponent},
-{path:'profil',component:ProfilComponent},
-
+{path:'profil',
+children:[{path:'view',component:ViewComponent},{path:'details',component:DetailsComponent}]
+},
+{path:'contact',redirectTo:'about',pathMatch:'full'},
+{path:'**',component:PageNotFoundComponent},
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,19 @@ import { HomeComponent } from './home/home.component';
 import { FormsModule } from '@angular/forms';
 import { AboutComponent } from './about/about.component';
 import { ProfilComponent } from './profil/profil.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { ViewComponent } from './view/view.component';
+import { DetailsComponent } from './details/details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     AboutComponent,
-    ProfilComponent
+    ProfilComponent,
+    PageNotFoundComponent,
+    ViewComponent,
+    DetailsComponent
 
   ],
   imports: [
